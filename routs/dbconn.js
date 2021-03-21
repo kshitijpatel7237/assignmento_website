2ea5e404d722462e0d4aa4eda3f99955ab4c9c20
kshitij <kshitijpatel917050@gmail.com>
2021-03-21 23:18:51 +0530

    Some helpful message for your future self

--- routs/dbconn.js
+++ routs/dbconn.js
@@ -18,8 +18,8 @@
 
 var mongoose=require('mongoose');
 var assert=require('assert');
-var url = "mongodb://localhost:27017/assignmento" || "mongodb+srv://user:ApKp@7237046763@cluster0.71j4n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ;
-
+var url = "mongodb+srv://user:ApKp@7237046763@cluster0.71j4n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ;
+//"mongodb://localhost:27017/assignmento" || 
 // connection code
 module.exports=mongoose.connect(
 	url,
